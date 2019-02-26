1be59ca82b94ebad6aea38e84205b95d14b64457
Blake Knight <oss.ideas@gmail.com>
2019-02-25 21:17:54 -0600

    :pencil: add `beforePaint` param to JSDoc comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { useEffect, useLayoutEffect, useState } from 'react';
 
 /**
  * Returns a state value representing if the component has mounted
+ * @param {Boolean} [beforePaint=false] if the effect should run synchronously
+ * in order to show before a browser paint.
  */
 function useHasMounted(beforePaint = false) {
   const [hasMounted, setHasMounted] = useState(false);
